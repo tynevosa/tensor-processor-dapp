e27716268396b9c532d018079380fcd58f3188cd
Mihir2423 <mihir.elitekods@gmail.com>
2024-04-09 22:09:30 +0530

    fix: default image instead of error image

--- app/(dashboard)/dashboard/models/page.tsx
+++ app/(dashboard)/dashboard/models/page.tsx
@@ -45,6 +45,8 @@ export default function Page() {
       setPage((prev) => prev + 1);
     }
   }, [inView]);
+  console.log(isPending);
+  
 
   useEffect(() => {
     if (modelPage?.length > 0) {
@@ -56,8 +58,8 @@ export default function Page() {
 
   return (
     <ScrollArea className="w-full h-full">
-      <div className="container px-1 py-12 w-full h-full">
-        <h1 className="font-bold text-3xl text-white lg:ml-6 ml-10">
+      <div className="px-1 py-12 w-full h-full container">
+        <h1 className="ml-10 lg:ml-6 font-bold text-3xl text-white">
           My Playground
         </h1>
         <div className="gap-6 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 mt-6 px-10 lg:px-6">
@@ -80,30 +82,31 @@ export default function Page() {
                     ref={ref}
                     className="flex gap-4 bg-[#121218] p-2 rounded-[8px]"
                   >
-                    <div className="w-40 h-40 min-w-40  relative">
+                    <div className="relative w-40 min-w-40 h-40">
                       <Image
                         alt="model"
                         src={cover_image_url ?? "/images/model.svg"}
-                        className="h-full absolute w-full"
+                        onError={({currentTarget}) => currentTarget.src = "/images/model.svg"}
+                        className="absolute w-full h-full"
                         width={500}
                         height={500}
                         priority
                       />
                     </div>
-                    <div className="flex flex-col justify-between flex-shrink flex-grow w-[calc(100%-176px)]">
-                      <div className="flex flex-col gap-4 items-stretch">
+                    <div className="flex flex-col flex-grow flex-shrink justify-between w-[calc(100%-176px)]">
+                      <div className="flex flex-col items-stretch gap-4">
                         <div className="flex gap-2 max-w-full">
-                          <Avatar className="rounded-none !w-5 ">
+                          <Avatar className="rounded-none !w-5">
                             <AvatarImage
-                              className="!w-5 !h-5 rounded-full"
+                              className="rounded-full !w-5 !h-5"
                               src="/images/model-avatar.png"
                             />
                           </Avatar>
-                          <h2 className="inline-block font-[600] text-sm text-white truncate flex-shrink">
+                          <h2 className="inline-block flex-shrink font-[600] text-sm text-white truncate">
                             {`${owner} / ${name}`}
                           </h2>
                         </div>
-                        <div className="font-[400] text-sm text-white max-h-10 overflow-hidden text-ellipsis">
+                        <div className="max-h-10 font-[400] text-ellipsis text-sm text-white overflow-hidden">
                           {description}
                         </div>
                       </div>
@@ -124,30 +127,31 @@ export default function Page() {
                     key={index}
                     className="flex gap-4 bg-[#121218] p-2 rounded-[8px]"
                   >
-                    <div className="w-40 h-40 min-w-40  relative">
+                    <div className="relative w-40 min-w-40 h-40">
                       <Image
                         alt="model"
                         src={cover_image_url ?? "/images/model.svg"}
-                        className="h-full absolute w-full"
+                        onError={({currentTarget}) => currentTarget.src = "/images/model.svg"}
+                        className="absolute w-full h-full"
                         width={500}
                         height={500}
                         priority
                       />
                     </div>
-                    <div className="flex flex-col justify-between flex-shrink flex-grow w-[calc(100%-176px)]">
-                      <div className="flex flex-col gap-4 items-stretch">
+                    <div className="flex flex-col flex-grow flex-shrink justify-between w-[calc(100%-176px)]">
+                      <div className="flex flex-col items-stretch gap-4">
                         <div className="flex gap-2 max-w-full">
-                          <Avatar className="rounded-none !w-5 ">
+                          <Avatar className="rounded-none !w-5">
                             <AvatarImage
-                              className="!w-5 !h-5 rounded-full"
+                              className="rounded-full !w-5 !h-5"
                               src="/images/model-avatar.png"
                             />
                           </Avatar>
-                          <h2 className="inline-block font-[600] text-sm text-white truncate flex-shrink">
+                          <h2 className="inline-block flex-shrink font-[600] text-sm text-white truncate">
                             {`${owner} / ${name}`}
                           </h2>
                         </div>
-                        <div className="font-[400] text-sm text-white max-h-10 overflow-hidden text-ellipsis">
+                        <div className="max-h-10 font-[400] text-ellipsis text-sm text-white overflow-hidden">
                           {description}
                         </div>
                       </div>
@@ -169,7 +173,7 @@ export default function Page() {
             "mt-[20%]": modelDatas.length === 0,
           })}
         >
-          {isPending && <Spinner />}
+          {isPending === "fetching" && <Spinner />}
         </div>
       </div>
     </ScrollArea>
