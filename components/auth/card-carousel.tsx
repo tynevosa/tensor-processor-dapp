aacb7a3b872fdf437cb67cb97eae1a04220ecd3f
Mihir2423 <mihir.elitekods@gmail.com>
2024-04-10 03:02:45 +0530

    feat: add auth card images data

--- components/auth/card-carousel.tsx
+++ components/auth/card-carousel.tsx
@@ -10,6 +10,59 @@ import {
 
 type Props = {};
 
+const carouselItems = [
+  {
+    runs: "50K",
+    name: "Cooperate Detective Cat",
+    desc: "Investigator detective cat, officially solving money laundering cases",
+  },
+  {
+    runs: "32K",
+    name: "Space Monkey - Scientist",
+    desc: "hi fidelity image, space monkey, investigator, NFt image",
+  },
+  {
+    runs: "110K",
+    name: "Pixel Art - Cyberpunk",
+    desc: "cyberpunk alien attack on new york city, pixel art",
+  },
+  {
+    runs: "20K",
+    name: "Abstract Pisces",
+    desc: "underwater world, imaginative creatures, pisces of different shape, planet and lifestyle",
+  },
+  {
+    runs: "89K",
+    name: "Asgard Defender",
+    desc: "abstract thor, god of ligthning, asgard throne, warriors and defenders of asgard nation",
+  },
+  {
+    runs: "5K",
+    name: "Futuristic Starbucks Stor",
+    desc: "abstract futuristic starbucks store, autobot delivery process, drone use",
+  },
+  {
+    runs: "98K",
+    name: "Mars Armageddon",
+    desc: "mars eclipse, thunderstorm, sandstorm, thunder rumbling,lightning, armageddon day on Mars",
+  },
+  {
+    runs: "13K",
+    name: "Red haired Hero",
+    desc: "Indian jones, demi god, tooth sayer, red haired warriors",
+  },
+  {
+    runs: "8K",
+    name: "Japanese Samurai Family",
+    desc: "Japanese family, inheritance, samurai ninja generation, snow filled house cold weather",
+  },
+  {
+    runs: "46K",
+    name: "Bumble Bee, Cypherpunked",
+    desc: "bumble bee, transformers cybepunk styled theme.",
+  },
+];
+
 export const CardCarousel = (props: Props) => {
   return (
     <div className="bg-[url(/backgrounds/card.png)] bg-cover bg-center p-4 rounded-[16px] w-[344px]">
@@ -26,26 +79,25 @@ export const CardCarousel = (props: Props) => {
           ]}
         >
           <CarouselContent className="">
-            {[1, 2, 3]?.map((item) => (
-              <CarouselItem key={item}>
+            {carouselItems?.map((item, i) => (
+              <CarouselItem key={i}>
                 <div className="relative">
                   <Image
                     alt="model"
-                    src="/images/model/model-1.png"
+                    src={`/images/model/model-${i+1}.png`}
                     width={312}
                     height={264}
                   />
                   <span className="top-1 right-4 absolute font-semibold text-white text-xl leading-6">
-                    50K runs
+                    {item.runs} runs
                   </span>
                 </div>
                 <div className="flex flex-col gap-3">
                   <h1 className="font-semibold text-white text-xl leading-6">
-                    Cooperate Detective Cat
+                    {item.name}
                   </h1>
                   <p className="font-semibold text-[#ABB5C9] text-sm leading-6">
-                    Investigator detective cat, officially solving money
-                    laundering cases
+                    {item.desc}
                   </p>
                 </div>
               </CarouselItem>
